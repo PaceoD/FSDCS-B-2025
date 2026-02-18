5f925c4919c7e67f20944e6d567a2a3a61ca16b9
ptomer40 <ptomer40@yahoo.com>
2026-02-18 10:26:33 +0530

    backend node

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const http=require('http');
 const PORT=4004;
 const apidata=require('./apiCalling')
-const server=http.createServer((req,res)=>{
+const dataWrite=require('./usefsmodule')
+const server=http.createServer(async (req,res)=>{
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 // res.setHeader('Content-Type','application/json');
 // res.end("<h2 style=color:red>Welcome to Node Server</h2>");
 if(req.url=="/msg" && req.method=="GET"){
@@ -20,7 +26,14 @@ res.end(JSON.stringify({msg:jsondata}))
 }
 else if(req.url=="/data" && req.method=="GET"){
 res.setHeader("Content-Type","application/json");
-      const jsondata=apidata(20,200);
+      const jsondata=await apidata();
+res.end(JSON.stringify({msg:jsondata}))
+
+}
+
+else if(req.url=="/datawrite" && req.method=="GET"){
+res.setHeader("Content-Type","application/json");
+      const jsondata=dataWrite();
 res.end(JSON.stringify({msg:jsondata}))
 
 }
